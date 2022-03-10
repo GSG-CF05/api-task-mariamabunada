10c53432a0f707d9ab89051c213e6c3dfb090a3e
mariamabunada <mariamabunada2000@gmail.com>
2022-03-10 18:00:55 +0200

    styling the API's data in the page relate #7

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let app=document.querySelector('#root')
 let container=document.createElement('div')
+container.setAttribute('class','container')
 app.appendChild(container)
-fetch('http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline')
+fetch('https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline')
 .then((res)=>{
     return res.json()
 })
@@ -14,22 +15,25 @@ fetch('http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline')
         brand.textContent=makeup.brand
         card.appendChild(brand)
         let name=document.createElement('p')
-        name.textContent=makeup.name
+        name.textContent=`Product name: ${makeup.name}`
         card.appendChild(name)
         let image_link=document.createElement('img')
         image_link.src=makeup.image_link
         card.appendChild(image_link)
         let price=document.createElement('p')
-        price.textContent=makeup.price
+        price.textContent=`Price: ${makeup.price}`
         card.appendChild(price)
         let description=document.createElement('p')
-        description.textContent=makeup.description
+        description.textContent=` Description: ${makeup.description}`
         card.appendChild(description)
         let rating=document.createElement('p')
-        rating.textContent=makeup.rating
+        rating.textContent=` Rating ${makeup.rating} stars`
         card.appendChild(rating)
         let product_type=document.createElement('p')
-        product_type.textContent=makeup.product_type
+        product_type.textContent=` Product type: ${makeup.product_type}`
         card.appendChild(product_type)
+        // set attributes
+        card.setAttribute('class','card')
+        
     });
     ;})
